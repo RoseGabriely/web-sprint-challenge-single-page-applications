23c7191ae110e0d91bbd7c805a0946f9232c1a5c
RoseGabriely <rosegabriely14@gmail.com>
2021-09-17 13:16:38 -0400

    created all form inputs

--- src/Form.js
+++ src/Form.js
@@ -8,6 +8,21 @@ export default function Form() {
         <label>
           Name:
           <input id="name-input" type="text" name="name" />
+          Select a size:
+          <select id="size-dropdown" name="size">
+            <option name=""></option>
+            <option name="size1">Small</option>
+            <option name="size2">Medium</option>
+            <option name="size3">Large</option>
+          </select>
+          Select Toppings:
+          <input type="checkbox" name="topping1" /> Corn
+          <input type="checkbox" name="topping2" /> Olives
+          <input type="checkbox" name="topping3" /> Extra Cheese
+          <input type="checkbox" name="topping4" /> Peppers Special
+          Instructions:
+          <input id="special-text" type="text" name="special" />
+          <button id="order-button">Add to Order</button>
         </label>
       </form>
     </>
